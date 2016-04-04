df1537063131a2965ddc7caac5ca0b599f87d5ad
Ameer Brown <akeithbrown@gmail.com>
2016-04-04 08:17:42 -0700

    need debugging

--- models/index.js
+++ models/index.js
@@ -3,5 +3,5 @@ mongoose.connect( process.env.MONGOLAB_URI ||
                   process.env.MONGOHQ_URL ||
                   "mongodb://localhost/personal-api");
 
-module.exports.Profile = require("./memories.js");
+module.exports.Profile = require("./profile.js");
 module.exports.Memory = require("./memories.js.");
